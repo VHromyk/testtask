e7da1ecec2cba053c68cd10e52fbbfa24e890503
Vitalii Hromyk <hromykk@gmail.com>
2022-06-06 00:23:50 +0300

    added the photo-cover

--- src/components/UserCard/UserCard.jsx
+++ src/components/UserCard/UserCard.jsx
@@ -1,22 +1,28 @@
 import style from './UserCard.module.scss';
 import normalizedStr from '../../utils/normalizedStr';
+import photoCover from '../../images/svg/photo-cover.svg';
 
 const UserCard = ({ user }) => {
+    const { name, email, phone, position, photo } = user;
+
     return (
         <div className={style.card}>
             <img
-                src={user.photo}
+                src={photo}
                 alt="avatar"
                 width="70px"
                 height="70px"
                 className={style.card_image}
+                // Check unloaded images
+                onError={(errorImg) => {
+                    errorImg.currentTarget.onerror = null; // prevents looping
+                    errorImg.currentTarget.src = photoCover;
+                }}
             />
-            <p className={style.card_element}>{normalizedStr(user.name, 34)}</p>
-            <p className={style.card_element}>
-                {normalizedStr(user.position, 34)}
-            </p>
-            <p>{normalizedStr(user.email, 34)}</p>
-            <p>{user.phone}</p>
+            <p className={style.card_element}>{normalizedStr(name, 34)}</p>
+            <p className={style.card_element}>{normalizedStr(position, 34)}</p>
+            <p>{normalizedStr(email, 34)}</p>
+            <p>{phone}</p>
         </div>
     );
 };
